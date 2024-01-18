ceedeededf61300881cdb00bd5ed848c189e324f
A-ri-SIM <149152400+A-ri-SIM@users.noreply.github.com>
2024-01-19 02:05:02 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // Header 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
-
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
   const scroll = window.scrollY;
   if (scroll > headerHeight) {
@@ -10,3 +9,10 @@ document.addEventListener('scroll', () => {
     header.classList.remove('header_dark');
   }
 });
+
+// scroll시 home 섹션 투명 스타일링
+const home = document.querySelector('.home_container');
+const homeHeight=home.offsetHeight;
+document.addEventListener('scroll',()=>{
+    home.style.opacity=1-window.scrollY/homeHeight;
+})
