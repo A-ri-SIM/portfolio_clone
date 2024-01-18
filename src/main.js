c7cc38b1a21e621b1a2e407c58b9d155603fa245
A-ri-SIM <149152400+A-ri-SIM@users.noreply.github.com>
2024-01-19 02:19:08 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
-  const scroll = window.scrollY;
-  if (scroll > headerHeight) {
+  if (window.scrollY > headerHeight) {
     header.classList.add('header_dark');
   } else {
     header.classList.remove('header_dark');
@@ -12,7 +11,17 @@ document.addEventListener('scroll', () => {
 
 // scroll시 home 섹션 투명 스타일링
 const home = document.querySelector('.home_container');
-const homeHeight=home.offsetHeight;
-document.addEventListener('scroll',()=>{
-    home.style.opacity=1-window.scrollY/homeHeight;
-})
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// arrow up 버튼 나타내기
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY>homeHeight/2){
+        arrowUp.style.opacity=1;
+    }else{
+        arrowUp.style.opacity=0;
+    }
+});
